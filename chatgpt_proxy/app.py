7180cf1d8d6fd2ea644e3a6383092b7bd899007a
Tuomo Kriikkula <tuokri@tuta.io>
2025-06-17 20:51:39 +0300

    Add basic API design, update deps

--- chatgpt_proxy/app.py
+++ chatgpt_proxy/app.py
@@ -19,3 +19,74 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+# Implements a simple proxy server for communication between an UnrealScript
+# client and OpenAI servers.
+
+import os
+from dataclasses import dataclass
+from types import SimpleNamespace
+from typing import TypeAlias
+
+import openai
+import sanic
+from sanic import Blueprint
+from sanic.request import Request
+from sanic.response import HTTPResponse
+
+api_v1 = Blueprint("api", version_prefix="/api/v", version=1)
+
+
+class Context(SimpleNamespace):
+    client: openai.AsyncOpenAI | None
+
+
+App: TypeAlias = sanic.Sanic[sanic.Config, Context]
+
+app: App = sanic.Sanic(__name__, ctx=Context())
+app.blueprint(api_v1)
+# app.config.OAS = False
+
+# Rough API design:
+# - /message: to "fire" actual message request -> returns a chat message to send in game.
+#    * all context data is taken into account
+# - /message_history: (context) post here to keep a short log of previous chat messages.
+# - /players: (context) post/delete here to keep the current player list up to date.
+# - /kills: (context) post here to keep a short log of previous kills.
+# - /reset_context: (context) reset all context data.
+# - /base_prompt: (context) appended in front of all requests to the LLM.
+
+# TODO: example prompt.
+"""
+{base_prompt}
+
+The game currently contains the following players
+"""
+
+
+@dataclass
+class MessageContext:
+    pass
+
+
+@api_v1.post("/game/<game_id>/message")
+async def post_game_message(request: Request, game_id: str, client: openai.AsyncOpenAI) -> HTTPResponse:
+    return sanic.text("TODO")
+
+
+@app.before_server_start
+async def before_server_start(app_: App, _):
+    api_key = os.environ.get("OPENAI_API_KEY")
+    client = openai.AsyncOpenAI(api_key=api_key)
+    app.ctx.client = client
+    app_.ext.dependency(client)
+
+
+@app.before_server_stop
+async def before_server_stop(app_: App, _):
+    if app_.ctx.client:
+        await app_.ctx.client.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True, dev=True)
